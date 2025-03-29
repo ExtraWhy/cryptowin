e41310d21d02d0ce6d299b6ba59a0e3841c14513
carnophager <carnophager@gmail.com>
2025-03-29 20:52:47 +0200

    fix: set proper api ports

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,8 +1,9 @@
-export const API_BASE_URL = process.env.BACKEND_URL || 'http://localhost:8080';
+export const API_BASE_URL = process.env.BACKEND_URL || 'http://localhost:8081';
+export const AUTH_BASE_URL = process.env.AUTH_URL || 'http://localhost:8080';
 export const FRONTEND_HOST = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 export const LOGIN_ROUTES = {
-  LOGIN_GOOGLE: `${API_BASE_URL}/auth/google/login?callback-url=${FRONTEND_HOST}`,
-  LOGIN_FACEBOOK: `${API_BASE_URL}/auth/facebook/login?callback-url=${FRONTEND_HOST}`,
-  LOGIN_APPLE: `${API_BASE_URL}/auth/apple/login`,
+  LOGIN_GOOGLE: `${AUTH_BASE_URL}/auth/google/login?callback-url=${FRONTEND_HOST}`,
+  LOGIN_FACEBOOK: `${AUTH_BASE_URL}/auth/facebook/login?callback-url=${FRONTEND_HOST}`,
+  LOGIN_APPLE: `${AUTH_BASE_URL}/auth/apple/login`,
 };
